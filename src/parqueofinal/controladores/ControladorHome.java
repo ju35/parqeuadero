d795a30d67a2dca865d4a851df685223c47eef41
“nombre <juaneslico2@gmail.com>
2021-11-25 16:57:16 -0500

    parqueadero

---
+++ src/parqueofinal/controladores/ControladorHome.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package parqueofinal.controladores;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import parqueofinal.modelos.Conductor;
+import parqueofinal.modelos.Vehiculos;
+import parqueofinal.vistas.VistaHome;
+import parqueofinal.vistas.VistaIngreso;
+import parqueofinal.vistas.VistaSalida;
+
+
+
+/**
+ *
+ * @author Hp
+ */
+public abstract class ControladorHome implements ActionListener{
+
+
+
+    
+    //ATRIBUTOS
+    VistaHome vistahome = new VistaHome();
+    VistaIngreso vistaingreso = new VistaIngreso();
+    VistaSalida vistasalida = new VistaSalida();
+    Vehiculos vehiculo = new Vehiculos();
+    Conductor conductor = new Conductor();
+    
+    
+
+    public ControladorHome(VistaHome vistahome,
+            Vehiculos vehiculo,Conductor conductor) {
+        
+        this.vehiculo=vehiculo;
+        this.conductor=conductor;
+        this.vistahome=vistahome;
+        vistahome.btnI.addActionListener(this);
+        vistahome.btnR.addActionListener(this);
+        vistahome.btnS.addActionListener(this);
+    
+    } 
+/*
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        btnI==VistaIngreso();
+       vistahome.btnI.setVisible(true);
+       
+    }
+*/        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+
+
+    
+    
+    
+    
